791384b535dd6b840b1c92ba62efda2ae993969e
Caio lima <caiohenrys50@gmail.com>
2024-04-13 14:24:21 -0300

    feat: add types children and subtext

--- src/components/infoWithIcon/index.tsx
+++ src/components/infoWithIcon/index.tsx
@@ -3,14 +3,28 @@ import { ReactNode } from "react";
 interface InfoWithIconProps {
   icon: ReactNode;
   text?: string | ReactNode;
+  children?: string | ReactNode;
+  subtext?: string | ReactNode;
   className: string;
 }
 
-export function InfoWithIcon({ icon, text, className }: InfoWithIconProps) {
+export function InfoWithIcon({
+  icon,
+  text,
+  subtext,
+  className,
+  children,
+}: InfoWithIconProps) {
   return (
     <div className="flex items-center gap-2">
       <div className={className}>{icon}</div>
-      {typeof text === "string" ? <p>{text}</p> : text}
+      <div className="flex flex-col text-base">
+        <div className="flex items-center gap-2">
+          {typeof text === "string" ? <p>{text}</p> : text}
+          <span>{children}</span>
+        </div>
+        <span>{subtext}</span>
+      </div>
     </div>
   );
 }
